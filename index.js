9c2601175ee209907cc5bb741a7e96e3da761fa5
Matthew Webb <mwebb2321@gmail.com>
2021-03-01 21:07:37 -0800

    starting on index.js file

---
+++ index.js
@@ -0,0 +1,9 @@
+const inquirer = require("inquirer");
+
+const intern = require("lib/intern");
+const employee = require("lib/employee");
+const engineer = require("engineer");
+const manager = require("lib.manager");
+
+const fs = require("fs");
+
